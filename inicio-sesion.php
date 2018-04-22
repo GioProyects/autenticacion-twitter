b04d71281d0e9bd904223cddd142079c25145b9e
GioProyects <giogow3halo@gmail.com>
2018-04-21 22:23:48 -0500

    pruebas

--- inicio-sesion.php
+++ inicio-sesion.php
@@ -5,7 +5,7 @@
     <title>Iniciado Sesion</title>
   </head>
   <body>
-    <button type="button" id="btnDatos">Get datos</button>
+    <button type="button"> <a href="twitter/tweets.php"></a> Get datos</button>
     <div id="resultados"></div>
 
   </body>
