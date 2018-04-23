a068f1b6b53ca336a5dbad29b20502dccdb9345f
GioProyects <giogow3halo@gmail.com>
2018-04-22 21:09:15 -0500

    mammsd

--- twitter/logout.php
+++ twitter/logout.php
@@ -1,7 +1,9 @@
 <?php
 
 session_start();
-unset ($SESSION['username']);
+unset($_SESSION['oauth_token']);
+unset($_SESSION['oauth_access_token']);
+unset($_SESSION['loggedin']);
 session_destroy();
 
 header('Location: https://logearse.herokuapp.com/index.php');
